bac9be1ef6d901b6932baffd16760d1c075306b8
esskayaitch <simonhillskh@gmail.com>
2021-03-20 21:20:51 +1100

    added dotenv

--- db.js
+++ db.js
@@ -1,8 +1,9 @@
+const dotenv = require('dotenv')
+dotenv.config()
 const mongodb = require('mongodb')
 
-const connectionString = 'mongodb+srv://todoApp1User:CwITI5RZBEDYlPiI@cluster0.dhphq.mongodb.net/OurComplexity?retryWrites=true&w=majority'
 
-mongodb.connect(connectionString, {useNewUrlParser: true, useUnifiedTopology: true}, function(err, client) {
+mongodb.connect(process.env.CONNECTIONSTRING, {useNewUrlParser: true, useUnifiedTopology: true}, function(err, client) {
   module.exports = client.db()
   const app = require('./app')
   app.listen(3003)
